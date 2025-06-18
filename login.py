de9ab69709184f2c7492b3b21168c9fce3dd9ba5
JDHA15 <josue.hernandez2026@hilasal.superate.org.sv>
2025-06-17 22:13:28 -0600

    Se actualizo login :memo:

--- login.py
+++ login.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 from flask_mysqldb import MySQL
 import bcrypt
 
 app = Flask(__name__)
+app.secret_key = 'llave'
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
@@ -26,9 +27,15 @@ def acceder():
     cursor.close()
 
     if usuario and bcrypt.checkpw(password, usuario[1].encode('utf-8')):
+         session['nombre'] = usuario[0]
         return render_template('LandPage.html', nombre=usuario[0])
     else:
         return 'Contraseña o correo incorrectos'
+        
+@app.route('/ajustes')
+def ajustes():
+    nombre = session.get('nombre') 
+    return render_template('settings.html', nombre = nombre)
 
 if __name__ == '__main__':
     app.run(debug=True)
